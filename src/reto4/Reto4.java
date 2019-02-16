c8466b30e0e3926c65a3318862ddb5e7caf2bf6e
unknown <al02775806@tecmilenio.mx>
2019-02-16 15:56:59 -0600

    Reto realizado en clase

---
+++ src/reto4/Reto4.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reto4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+public class Reto4 {
+
+    
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      char opcion;
+      double costo_boleto = 0.0;
+      
+      do{
+          System.out.println("Indique el tipo de boleto: ");
+          opcion = sc.next().charAt(0);
+          switch(opcion){
+              case 'A':
+              case 'a':
+                  costo_boleto = 100.00;
+                  break;
+              case'b':
+              case 'B':
+                  costo_boleto = 70.00;
+                  break;
+              case 'i':
+              case 'I':
+                  costo_boleto = 50.00;
+                  break;
+              case 'n':    
+              case 'N':
+                  costo_boleto = 70.00;
+                  break;
+              default:
+                  System.out.println("No existe esa opción");
+          }
+          System.out.println("Costo del boleto es: " + costo_boleto);
+          System.out.println("¿Desea otro boleto (s/n)? ");
+          opcion = sc.next().charAt(0);
+      }while(opcion != 'n');
+    }
+ 
+    
+}
---
+++ src/reto4/SegundoPrograma.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reto4;
+
+/**
+ *
+ * @author AlumnoUTM
+ */
+public class SegundoPrograma {
+    public String nombre;
+    public int edad;
+    public double altura;
+    
+    public void Saludar(){   
+    }
+    
+    public void Despedirse(){     
+    }
+    
+    public void Retirarse(){
+    }
+    
+}
